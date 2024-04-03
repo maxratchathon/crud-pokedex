5bcca096017b7e54ed5ebcb6f9fb82c3a7fd6cd3
Maxmerock123 <Maxmerock@gmail.com>
2024-04-04 00:16:36 +0700

    Improve Navbar

--- frontend/components/NavBar.js
+++ frontend/components/NavBar.js
@@ -1,8 +1,8 @@
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import pokemonIcon from "../img/pokemonIcon.png";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const navigation = [
   // { name: "Add Pokemon", href: "#", current: true },
@@ -36,12 +36,13 @@ export default function NavBar() {
                 </Disclosure.Button>
               </div>
               <div className="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
-                <div className="flex items-center flex-shrink-0">
+                <div className="flex items-center flex-shrink-0 gap-2">
                   <img
                     className="w-auto h-8"
-                    src={pokemonIcon}
-                    alt="Your Company"
+                    src="/pokemonIcon.png"
+                    alt="Pokemon"
                   />
+                  <p className="text-white">CRUD Pokedex</p>
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
@@ -79,11 +80,7 @@ export default function NavBar() {
                     <Menu.Button className="relative flex text-sm bg-gray-800 rounded-full focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="absolute -inset-1.5" />
                       <span className="sr-only">Open user menu</span>
-                      <img
-                        className="w-8 h-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
+                      <RxHamburgerMenu size={30} className="text-gray-400" />
                     </Menu.Button>
                   </div>
                   <Transition
